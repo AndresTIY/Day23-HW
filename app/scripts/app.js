8e2bc7cb1669d76ec27aa6f7529c3c89db788615
Andres Cifuentes <aacifuentes@gmail.com>
2017-03-29 21:23:19 -0500

    working on C

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,9 +2,140 @@ import Store from './store.js'
 
 export default function app() {
   //All of your code starts here
+  const urlBlog = 'http://tiny-za-server.herokuapp.com/collections/dres-blog';
+  let $blogView =
+    $(`<div>
+        <input id="blog-title" placeholder="title" type="text" name="" value=""><br>
+        <textarea id="body-input" name="name" rows="8" cols="80"></textarea><br>
+        <button id="submitBtn" type="button" name="button">Submit</button>
+      </div>`);
+  let submitBtn = $($blogView).find('#submitBtn');
+  let $infoView =
+    $(`<div>
+      <h3>Give Me Your Info</h3>
+      <input id="first-name" placeholder="First Name" type="text" name="" value=""><br>
+      <input id="last-name" placeholder="Last Name" type="text" name="" value=""><br>
+      <input id="phone-num" placeholder="Phone Number" type="text" name="" value=""><br>
+      <input id="address" placeholder="Address" type="text" name="" value=""><br>
+      <button id="info-submit" type="button" name="button">Submit Info</button>
+    </div>`)
+  let infoBtn = $($infoView).find('#info-submit');
+  let $blogViewAll =
+    $(`<div class="blog-view">
+        <h3>Check Out These Blogs</h3>
+        <div class="column-1">
+          <ul class="link-list"></ul>
+        </div>
+        <div class="column-2"></div>
+      </div>`);
+  let $findUl = $($blogViewAll).find('.link-list');
+  let $blogBody = $($blogViewAll).find('.column-2');
+  var $findLi;
+
+ // = $($blogViewAll).find('.link');
+
+
+  let settings = {
+  	type: 'GET',
+  	dataType: 'json',
+  	url: urlBlog
+    };
+
+
+//---------------Button Clicks---------------
+  submitBtn.on('click', function(e){
+    let $blogTitle = $($blogView).find('#blog-title').val();
+    let $blogBody = $($blogView).find('#body-input').val();
+    $.ajax({
+      type: 'POST',
+      contentType: 'application/json',
+      url: urlBlog,
+      data: JSON.stringify({
+        title: $blogTitle,
+        body: $blogBody
+      })
+    }).then(function(data, status, xhr){
+      console.log(status);
+    })
+
+  })
+  infoBtn.on('click', function(e){
+    let $firstName = $($infoView).find('#first-name').val();
+    let $lastName = $($infoView).find('#last-name').val();
+    let $phoneNum = $($infoView).find('#phone-num').val();
+    let $address = $($infoView).find('#address').val();
+    $.ajax({
+      type: 'POST',
+      contentType: 'application/json',
+      url: urlBlog,
+      data: JSON.stringify({
+        firstName: $firstName,
+        lastName: $lastName,
+        phoneNumber: $phoneNum,
+        address: $address
+      })
+    }).then(function(data, status){
+      let $nameDisplay = $(`<h3>Hello ${$firstName}!</h3>`);
+      $('#app').append($nameDisplay);
+      $($infoView).addClass('hide');
+      console.log(status);
+    })
+  })
+  $('#delete-all').on('click', function(e){
+  	$.ajax(settings).then(function(data, status, xhr){
+  		data.forEach(function(item, i, arr){
+  			let id = item._id;
+  			let url = `${urlBlog}/${id}`;
+  			$.ajax({
+  				type: 'DELETE',
+  				url: url
+  			})
+  		})
+  	})
+  })
+
+//---------------Get Links-------------------
+  function getLinks (){
+  let blogLinks;
+  $.ajax(settings).then(function(data, status, xhr){
+    data.forEach(function(item, i, arr){
+      let id = item._id;
+      if (item.title){
+        blogLinks = `<li><a class="link" href=#${item.title}>${item.title}</a></li>`
+        $($findUl).append(blogLinks);
+      }
+    })
+  })
+  // return $findLi = $(blogLinks).find('.link');
+}
+
+
+//----------------Display Link Content
+//when link get's clicked, body needs to be displayed under $blogBody
+
+$findUl.on('click','.link', function(e){
+  var title = $(e.currentTarget).text();
+  console.log(title);
+});
+
+
+//
+// let bodyPost = function(){
+//   $.ajax(settings).then(function(data, status, xhr){
+//     data.forEach(function(item, i, arr){
+//
+//     })
+//   })
+// }
+
+
+
+
+
+
 
   //Example of the most minimal view possible.
-  let defaultView = `<h1>Hello World!</h1>`
+  let defaultView = `<h1>Let's Blog!</h1>`
 
   //The default state of this app is nothing, just an empty object.
   //  Don't worry about adding state until you need to keep track of it across views.
@@ -37,6 +168,11 @@ export default function app() {
   const render = function (state, event, data) {
     // You will want to update this render function to render
     $('#app').html(defaultView);
+    $('#app').append($blogView);
+    $('#app').append($infoView);
+    $('#app').append($blogViewAll);
+    getLinks();
+
   };
 
   // Every time an event is fired the update function will run
--- readme.md
+++ readme.md
@@ -1,4 +1,35 @@
+## Day 23 Homework
 
-## Description
+#### Part 1
+Requirements
+- Create a form that saves a blog post to the tiny-za server
+(http://tiny-za-server.herokuapp.com/collections/dres-blog)
+- Must contain a Title and Body
+- Doesn't need to display anything, just save to server
+- HTML should exist in view function
 
-> Describe your application here and what it does
+#### Part 2
+Requirements
+- Create a form for a person
+  - Include: First Name, Last Name, Address, Phone #
+- Must be saved to the server
+- After submitting this info, screen should display "Hello PERSON_NAME"
+-
+#### Part 3
+Requirements
+- Create a "Blog Reading App"
+- Must have 2 columns
+  - Left Column has links to different blog posts
+  - Right Column contains Blog Title and Body
+  - WHEN: Blog on left is clicked
+  - WHAT: Blog is displayed on right
+
+- start with creating two divs side by side
+  - everything in body is centered already
+  - have left column be smaller than right
+- Questions so far:
+  - In order to get links, I need to get the url ID
+  - How do I retrieve the link? Must put it in an anchor tag. Perhaps as a list element.
+  - GET LINKS/ID BEFORE I LEAVE TODAY
+
+c) Create a "blog reading app", where you have two columns. A list of blog posts on the side and a place to read the posts in the middle. When you click on a post title, it "opens" that post by placing its contents in the middle column, but does not remove the list from the page.
